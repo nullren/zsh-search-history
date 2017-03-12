40e786eeb3c0e809df7fa26b9e0870336c82000d
Renning Bruns <chopflop@gmail.com>
2017-03-12 16:38:44 -0700

    restructured it

--- lambdas/zsh-api-create-api-key.js
+++ lambdas/zsh-api-create-api-key.js
@@ -1,71 +1,90 @@
-// zsh-signup.js
 'use strict';
 
 console.log('Loading function');
 
 const AWS = require('aws-sdk');
-
 const apigateway = new AWS.APIGateway();
 const dynamodb = new AWS.DynamoDB();
 const ses = new AWS.SES({region: "us-west-2"});
 
+function fail(message, err, awsCb) {
+    console.error(message, err, err.stack);
+    awsCb(err, message);
+}
+
+function successfulResponse(email, awsCb) {
+    awsCb(null, {
+        statusCode: '200',
+        body: 'Check ' + email + ' for your API key'
+    });
+}
+
 exports.handler = (event, context, callback) => {
     console.log('Received event:', JSON.stringify(event, null, 2));
-
     const payload = JSON.parse(event.body);
-    const params = { name: payload.email, description: "User created in Sign-up API", enabled: true }
+    createApiKey(payload.email, callback);
+};
 
+// the chain gang follows
+
+function createApiKey(email, awsCb) {
+    const params = { name: email, description: "User created in Sign-up API", enabled: true };
     apigateway.createApiKey(params, (err, data) => {
-        if (err) {
-            console.log("Failed to create API Key", err, err.stack); // an error occurred
-            callback(err, 'Failed to create API Key');
-        }
+        if (err) fail('Failed to create API Key', err, awsCb);
         else {
-            console.log("API Key created", data);           // successful response
+            console.log("API Key created", data);
+            const api_key_id = data.id;
             const api_key = data.value;
             const email = data.name;
             // associate key to usage plan
-            apigateway.createUsagePlanKey({ keyId: data.id, keyType: "API_KEY", usagePlanId: "0nxuwr" }, (err, data) => {
-                if (err) {
-                    console.log("Failed to add key to usage plan", err, err.stack); // an error occurred
-                    callback(err, 'Failed to add key to usage plan');
-                }
-                else {
-                    console.log("API Key added to usage plan", data);           // successful response
-                    // dynamodb row
-                    const ddbRecord = { Item: { "email": { S: email }, "apiKey": { S: api_key } }, TableName: "zsh-history-api-keys" };
-                    dynamodb.putItem(ddbRecord, (err, data) => {
-                        if (err) {
-                            console.log('Failed to add row to DynamoDB', err, err.stack); // an error occurred
-                            callback(err, 'Failed to add row to DynamoDB');
-                        }
-                        else {
-                            console.log('Added row to DynamoDB', data);           // successful response
-                            const emailParams = {
-                                Destination: { ToAddresses: [email] },
-                                Message: {
-                                    Body: { Text: { Data: api_key } },
-                                    Subject: { Data: "Your ZSH History API key" }
-                                },
-                                Source: "zsh-history@omgren.com"
-                            };
-                            ses.sendEmail(emailParams, (err, data) => {
-                                if (err) {
-                                    console.log("Failed to send email", err, err.stack); // an error occurred
-                                    callback(err, 'Failed to send email');
-                                }
-                                else {
-                                    console.log("Sent email", data);           // successful response
-                                    callback(null, {
-                                        statusCode: '200',
-                                        body: 'Check ' + email + ' for your API key'
-                                    });
-                                }
-                            });
-                        }
-                    });
-                }
-            });
+            attachUsagePlan(api_key_id, api_key, email, awsCb);
         }
     });
-};
+}
+
+function attachUsagePlan(api_key_id, api_key, email, awsCb) {
+    const params = { keyId: api_key_id, keyType: "API_KEY", usagePlanId: "0nxuwr" };
+    apigateway.createUsagePlanKey(params, (err, data) => {
+        if (err) fail('Failed to add key to usage plan', err, awsCb);
+        else {
+            console.log("API Key added to usage plan", data);
+            storeInDdb(email, api_key, awsCb);
+        }
+    });
+}
+
+function storeInDdb(email, api_key, awsCb) {
+    const ddbRecord = {
+        Item: { "email": { S: email }, "apiKey": { S: api_key } },
+        TableName: "zsh-history-api-keys"
+    };
+    dynamodb.putItem(ddbRecord, (err, data) => {
+        if (err) fail('Failed to add row to DynamoDB', err, awsCb);
+        else {
+            console.log('Added row to DynamoDB', data);
+            sendEmail(email, api_key, awsCb);
+        }
+    });
+}
+
+function sendEmail(email, api_key, awsCb) {
+    const emailParams = {
+        Destination: { ToAddresses: [email] },
+        Message: {
+            Body: { Text: { Data: api_key } },
+            Subject: { Data: "Your ZSH History API key" }
+        },
+        Source: "zsh-history@omgren.com"
+    };
+    ses.sendEmail(emailParams, sesEmailCb(email, awsCb));
+}
+
+function sesEmailCb(email, awsCb) {
+    return (err, data) => {
+        if (err) fail("Failed to send email", err, awsCb);
+        else {
+            console.log("Sent email", data);
+            successfulResponse(email, awsCb);
+        }
+    };
+}
